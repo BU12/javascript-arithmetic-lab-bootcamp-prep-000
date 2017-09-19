023cf2f217d97883880e10c02bccc4e26878cab0
BU12 <williampeterson23@gmail.com>
2017-09-19 01:24:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,44 @@
+var a = 656
+var b = 378
+function add(a, b) {
+  console.log(a + b)
+}
+
+function subtract(a, b) {
+  console.log(a - b)
+}
+function multiply(a, b) {
+  console.log(a * b)
+}
+
+function divide(a, b) {
+  console.log(a / b)
+}
+
+function increment(n) {
+  console.log(n += 1)
+}
+
+function decincrement(n) {
+  console.log(n -= 1)
+}
+
+function makeInt(n) {
+  console.log(parseInt(n))
+}
+
+function makeIntBaseTen(n) {
+  console.log(10 + parseInt(n))
+}
+
+function makeIntNaN() {
+  console.log(parseInt('nope!'))
+}
+
+function preserveDecimal(n) {
+  console.log(parseFloat(n))
+}
+
+function preserveDecimalNaN() { //works
+  console.log(parseFloat('nope!'))
+}
